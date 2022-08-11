5dbfe4e69fd32385668aeab206eac13ce361bd59
Fer Torres <fertorres638@gmail.com>
2022-08-11 13:23:42 -0500

    editar html

---
+++ lab1/lab3/pag.js
@@ -0,0 +1,29 @@
+console.warn("Cuidado")
+
+console.info("Esto es información")
+
+console.assert(1 === 1)
+
+console.error("Error")
+
+//Triple igual compara valor y tipo de dato
+//Doble igual compara valor únicamente
+
+alert("Esto es una alerta")
+
+const nombre = prompt("Cómo te llamas?")
+
+const is_somnoliento = confirm("Tienes sueño?")
+
+if (is_somnoliento) {
+    alert("Hay que dormir mejor hoy");
+} else {
+    alert("Has dormido bien estos días.")
+}
+
+//arreglo asociativo
+
+menu["postre"] = "Ate con queso"
+boton.onclick = () => {
+    document.getElementById("contenedor_imagen").innerHTML = '';
+} 
