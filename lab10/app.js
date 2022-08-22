2ade6e0aadfee7bca13fa4df7849d9e17da7c303
Fer Torres <fertorres638@gmail.com>
2022-08-22 13:00:35 -0500

    laboratorio de ruteo

---
+++ lab10/app.js
@@ -0,0 +1,59 @@
+setTimeout(() => console.log('codigo asincrono'), 4000);
+
+const filesystem = require('fs');
+filesystem.writeFileSync('hola.txt', 'hola desde node');
+
+const arreglo = [5000, 600, 90, 80, 7000, 500, 300, 200, 500, 20000];
+
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
+
+//crear un servidor 
+const http = require('http');
+
+//request.url es para obtener la url de la petición
+//request.socket.remoteAddress es para obtener la dirección IP 
+const server = http.createServer( (request, response) => {
+    console.log(request.url)
+    console.log(request.socket.remoteAddress)
+
+    if(request.url === "/hola") {
+    response.setHeader('Content-Type', 'text/html'); //envía el sobre
+    response.write("<!DOCTYPE html>");
+    response.write('<head><meta charset="utf-8"></head>');
+    response.write("<h1>Hola!</h1>"); //contenido de la carta 
+    response.end(); //hace que respuesta se envíe al cliente 
+    }  else if (request.url === "/trivia" && request.method === "GET" /*obtener un recurso*/) {
+        response.setHeader('Content-Type', 'text/html'); //envía el sobre
+        response.write("<!DOCTYPE html>");
+        response.write('<head><meta charset="utf-8"></head>');
+        response.write("<h1>Pregunta algo</h1>"); //contenido de la carta
+        response.write('<form action="trivia" method="POST">'); 
+        response.write('<fieldset>');
+        response.write("<legend>Equipos</legend>");  
+        response.write('<label for="visitante">Visitante</label><input type="text" name="visitante" id="visitante'); //id sirve del lado del cliente, name del lado del servidor
+        response.write('<label for="local">Local</label><input type="text" name="local" id="local');
+        response.write('</fieldset>') //agrupa en una caja
+        response.write("</form>"); 
+        response.end(); //hace que respuesta se envíe al cliente 
+        } else if (request.url === "/trivia" && request.method === "POST" /*enviar info (cuando quieras una respuesta luego de picarle al botón*/) {
+            response.setHeader('Content-Type', 'text/html'); //envía el sobre
+            response.write("<!DOCTYPE html>");
+            response.write('<head><meta charset="utf-8"></head>');
+            response.write("<h1>El resultado es: </h1>"); //contenido de la carta
+            response.write("</form>"); 
+            response.end(); 
+            } else {
+        response.setHeader('Content-Type', 'text/html'); //envía el sobre
+        response.write("<!DOCTYPE html>");
+        response.write('<head><meta charset="utf-8"></head>');
+        response.statusCode = 404;
+        response.write("<h1>El documento solicitado no existe</h1>");
+        response.end();
+    }
+});
+
+server.listen(5000);
---
+++ lab10/pag.js
@@ -0,0 +1,101 @@
+// console.warn("Cuidado")
+
+// console.info("Esto es información")
+
+// console.assert(1 === 1)
+
+// console.error("Error")
+
+// //Triple igual compara valor y tipo de dato
+// //Doble igual compara valor únicamente
+
+// alert("Esto es una alerta")
+
+// const nombre = prompt("Cómo te llamas?")
+
+// const is_somnoliento = confirm("Tienes sueño?")
+
+// if (is_somnoliento) {
+//     alert("Hay que dormir mejor hoy");
+// } else {
+//     alert("Has dormido bien estos días.")
+// }
+
+
+// boton.onclick = () => {
+//     console.log("Click!");
+//     document.getElementById("contenedor_imagen").innerHTML = 
+//         '<img src="media/Lucky.jpg" alt="Lucky">';
+// }
+
+const boton_cambio_perrito = document.getElementById("cambiar_perrito");
+
+boton_cambio_perrito.onclick = () => {
+    console.log("click funciona");
+    const imagen = document.getElementById("imagen");
+    if (imagen.src == 'media/Lucky.jpg'){
+        imagen.src = 'media/Moka.jpg';
+    }
+    
+}
+
+//let is_happy = false;
+
+//const foto1 = () => {
+//     const contenedor = document.getElementById("contenedor_imagen");
+//     contenedor.innerHTML = '<img src="media/Moka.jpg" alt="Moka" width="400">';
+//     boton_cambio_perrito.innerText = "Moka";
+//     boton_cambio_perrito.style.backgroundColor = "#f48fb1";
+//     //is_happy = true;
+//     document.body.style.color = "#f48fb1";
+// }
+
+const foto2 = () => {
+    const contenedor = document.getElementById("contenedor_imagen");
+    contenedor.innerHTML = '<img src="media/Lucky.jpg" alt="Lucky" width="400">';
+    boton_cambio_perrito.innerText = "Lucky";
+    boton_cambio_perrito.style.backgroundColor = "#01579b";
+    //is_happy = false;
+}
+
+const foto3 = () => {
+    const contenedor = document.getElementById("contenedor_imagen");
+    contenedor.innerHTML = '<img src="media/Rocky.jpg" alt="Lucky" width="400">';
+    boton_cambio_perrito.innerText = "Rocky";
+    boton_cambio_perrito.style.backgroundColor = "#01579b";
+    //is_happy = false;
+}
+
+const foto4 = () => {
+    const contenedor = document.getElementById("contenedor_imagen");
+    contenedor.innerHTML = '<img src="media/Toby.jpg" alt="Lucky" width="400">';
+    boton_cambio_perrito.innerText = "Toby";
+    boton_cambio_perrito.style.backgroundColor = "#01579b";
+    //is_happy = false;
+}
+
+//Default de la página
+foto2();
+
+boton_cambio_perrito.onclick = () => {
+    if (foto1) {
+        foto2();
+    } else if (foto2) {
+        foto3();
+    } else {
+        foto4()
+    }
+}
+
+//Una función que reciba un arreglo de números y devuelva su promedio.
+function promedio(arreglo) {
+    let suma = 0;
+    for(let i of arreglo) {
+        suma = suma + i;
+        return suma;
+    }
+    let avg = suma/arreglo.length;
+    return avg;
+}
+
+console.log("el promedio es", promedio(arreglo))
